a3e2ea7eef498b6270c363a4989b511492092ceb
n.fridman <n.fridman@burning-lab.com>
2022-10-31 08:46:30 +0400

    SM-3 Create scene load type enumeration.

---
+++ Assets/BurningLab/SceneManagement/Runtime/Types/SceneLoadType.cs
@@ -0,0 +1,19 @@
+﻿namespace BurningLab.SceneManagement.Types
+{
+    /// <summary>
+    /// Scene load type.
+    /// </summary>
+    [System.Serializable]
+    public enum SceneLoadType
+    {
+        /// <summary>
+        /// Load scene as active.
+        /// </summary>
+        Active = 0,
+        
+        /// <summary>
+        /// Load scene as additional.
+        /// </summary>
+        Additional = 1,
+    }
+}
