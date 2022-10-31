12b4241be44f4bd7f6efc3a063a80d319f1b6109
n.fridman <n.fridman@burning-lab.com>
2022-10-31 08:43:47 +0400

    SM-2 Create scene asset type enumeration.

---
+++ Assets/BurningLab/SceneManagement/Runtime/Types/SceneAssetType.cs
@@ -0,0 +1,18 @@
+﻿namespace BurningLab.SceneManagement.Types
+{
+    /// <summary>
+    /// Scene asset type. Local or addressable scene.
+    /// </summary>
+    public enum SceneAssetType
+    {
+        /// <summary>
+        /// Local scene asset.
+        /// </summary>
+        LocalAsset = 0,
+        
+        /// <summary>
+        /// Addressable scene asset.
+        /// </summary>
+        AddressableAsset = 1,
+    }
+}
