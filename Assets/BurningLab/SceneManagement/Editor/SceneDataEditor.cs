eacda4492f1399eb87b36642018011079fa56158
n.fridman <n.fridman@burning-lab.com>
2022-11-01 04:21:19 +0400

    SM-12 Create scene data editor.

---
+++ Assets/BurningLab/SceneManagement/Editor/SceneDataEditor.cs
@@ -0,0 +1,57 @@
+﻿using BurningLab.SceneManagement.Database;
+using BurningLab.SceneManagement.Types;
+using UnityEditor;
+
+namespace BurningLab.SceneManagement.Editor
+{
+    [CustomEditor(typeof(SceneData))]
+    public class SceneDataEditor : UnityEditor.Editor
+    {
+        private SerializedProperty _sceneAssetField;
+        private SerializedProperty _sceneAssetReferenceField;
+        private SerializedProperty _loadPriorityField;
+        private SerializedProperty _assetTypeField;
+        private SerializedProperty _sceneLoadTypeField;
+        private SerializedProperty _loadModeField;
+        private SerializedProperty _activationModeField;
+
+        private void OnEnable()
+        {
+            _sceneAssetField = serializedObject.FindProperty("_sceneAsset");
+            _sceneAssetReferenceField = serializedObject.FindProperty("_sceneAssetReference");
+            _loadPriorityField = serializedObject.FindProperty("_loadPriority");
+            _assetTypeField = serializedObject.FindProperty("_assetType");
+            _sceneLoadTypeField = serializedObject.FindProperty("_sceneLoadType");
+            _loadModeField = serializedObject.FindProperty("_loadMode");
+            _activationModeField = serializedObject.FindProperty("_activationMode");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            SceneAssetType sceneAssetType = (SceneAssetType) _assetTypeField.enumValueIndex;
+
+            EditorGUILayout.LabelField("Settings", EditorStyles.boldLabel);
+            
+            switch (sceneAssetType)
+            {
+                case SceneAssetType.LocalAsset:
+                    EditorGUILayout.PropertyField(_sceneAssetField);
+                    break;
+                
+                case SceneAssetType.AddressableAsset:
+                    EditorGUILayout.PropertyField(_sceneAssetReferenceField);
+                    break;
+            }
+
+            EditorGUILayout.PropertyField(_loadPriorityField);
+            EditorGUILayout.PropertyField(_assetTypeField);
+            EditorGUILayout.PropertyField(_sceneLoadTypeField);
+            EditorGUILayout.PropertyField(_loadModeField);
+            EditorGUILayout.PropertyField(_activationModeField);
+            
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
