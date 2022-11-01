5eea6616a0079ef130aa16cf6826b7cd7decb89f
n.fridman <n.fridman@burning-lab.com>
2022-11-01 04:06:20 +0400

    SM-9 Create scene asset field property drawer.

---
+++ Assets/BurningLab/SceneManagement/Editor/SceneAssetFieldPropertyDrawer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using BurningLab.SceneManagement.Attributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace BurningLab.SceneManagement.Editor
+{
+    [CustomPropertyDrawer(typeof(SceneAssetField))]
+    public class SceneAssetFieldPropertyDrawer : PropertyDrawer
+    {
+        private SceneAsset _sceneAsset;
+        
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (string.IsNullOrEmpty(property.stringValue) == false)
+            {
+                _sceneAsset = AssetDatabase.LoadAssetAtPath<SceneAsset>(property.stringValue);
+            }
+
+            _sceneAsset = EditorGUI.ObjectField(position, label, _sceneAsset, typeof(SceneAsset), true) as SceneAsset;
+
+            if (_sceneAsset != null)
+            {
+                property.stringValue = AssetDatabase.GetAssetPath(_sceneAsset);
+            }
+            else
+            {
+                property.stringValue = String.Empty;
+            }
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return base.GetPropertyHeight(property, label);
+        }
+    }
+}
