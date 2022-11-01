30a77655b4271a6b66e779592ecaaf5f422e3622
n.fridman <n.fridman@burning-lab.com>
2022-11-01 04:04:44 +0400

    SM-8 Create scene asset field property attribute.

---
+++ Assets/BurningLab/SceneManagement/Runtime/Attributes/SceneAssetField.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace BurningLab.SceneManagement.Attributes
+{
+    /// <summary>
+    /// Draw string field as scene asset selector.
+    /// </summary>
+    public class SceneAssetField : PropertyAttribute
+    {
+        
+    }
+}
