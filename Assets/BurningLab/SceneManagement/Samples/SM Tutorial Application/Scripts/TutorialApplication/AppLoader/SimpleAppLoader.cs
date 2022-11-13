4f6e1b2d9284e61487326be90d3f29704356afd3
n.fridman <n.fridman@burning-lab.com>
2022-11-13 07:37:02 +0400

    Add preloader logic.

--- Assets/BurningLab/SceneManagement/Samples/SM Tutorial Application/Scripts/TutorialApplication/AppLoader/SimpleAppLoader.cs
+++ Assets/BurningLab/SceneManagement/Samples/SM Tutorial Application/Scripts/TutorialApplication/AppLoader/SimpleAppLoader.cs
@@ -1,21 +1,77 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using BurningLab.SceneManagement;
+using BurningLab.SceneManagement.Samples.SM_Tutorial_Application.Scripts.TutorialApplication.AppLoader.Types;
+using BurningLab.SceneManagement.Types;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SimpleAppLoader : MonoBehaviour
 {
-    [Header("Settings")]
-    [SerializeField] private List<string> _scenesToLoad;
+    [Header("Settings")] [SerializeField] private float _delay;
+    [SerializeField] private List<LoadStage> _stages;
+
+    public int StagesCount => _stages.Count;
+
+    public event Action<LoadStage> OnStartStage;
+    public event Action<LoadStage> OnEndStage;
+
+    private ScenesLoadOperation _operation;
     
-    private void Start()
+    private bool LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+        return true;
+    }
+    
+    private IEnumerator Start()
     {
         DontDestroyOnLoad(gameObject);
-        
-        foreach (string sceneName in _scenesToLoad)
+
+        foreach (LoadStage stage in _stages)
         {
-            SceneManager.LoadScene(sceneName, LoadSceneMode.Additive);
+            OnStartStage?.Invoke(stage);
+            Debug.Log($"Start stage: {stage.stageName}");        
+            Debug.Log(stage.type);
+            switch (stage.type)
+            {
+                case LoadStageType.Delay:
+                    yield return new WaitForSeconds(stage.delay);
+                    break;
+                
+                case LoadStageType.LoadScene:
+                    yield return LoadScene(stage.sceneName);
+                    break;
+                
+                case LoadStageType.LoadScenesGroup: 
+                    _operation = ScenesSwitcher.Instance.LoadScenesGroup(stage.scenesGroup);
+                    while (_operation.IsDone == false)
+                    {
+                        yield return new WaitForSeconds(0.1f);
+                    }
+                    break;
+                
+                case LoadStageType.ScenesGroupActivation:
+                    _operation.ActivateScenes();
+                    break;
+                
+                case LoadStageType.WaitAnyKeyDown:
+                    while (Input.anyKeyDown == false)
+                    {
+                        if (Input.anyKey)
+                        {
+                            break;
+                        }
+                        yield return new WaitForEndOfFrame();
+                    }
+                    break;
+            }
+            
+            OnEndStage?.Invoke(stage);
         }
-        
+
+        yield return new WaitForSeconds(_delay);
         Destroy(gameObject);
     }
 }
