71242288f7824fb611ef1eab94ebb0778bbf1aab
n.fridman <n.fridman@burning-lab.com>
2023-08-04 01:56:06 +0300

    Update IScene Data,

--- Assets/BurningLab/SceneManagement/Runtime/Database/Data/ISceneData.cs
+++ Assets/BurningLab/SceneManagement/Runtime/Database/Data/ISceneData.cs
@@ -9,10 +9,29 @@ namespace BurningLab.SceneManagement.Database
     /// </summary>
     public interface ISceneData
     {
+        #region Public Methods
+
+        #region Local Scene Path Methods
+
+        /// <summary>
+        /// Set target scene to scene data instance.
+        /// </summary>
+        /// <param name="scenePath">Target scene path.</param>
+        public void SetScene(string scenePath);
+
         /// <summary>
         /// Scene asset path.
         /// </summary>
         public string GetSceneAssetPath();
+        
+        /// <summary>
+        /// Scene asset name.
+        /// </summary>
+        public string GetSceneAssetName();
+        
+        #endregion
+
+        #region Addressable Scene Asset Methods
 
         /// <summary>
         /// Reference to addressable scene asset.
@@ -20,39 +39,85 @@ namespace BurningLab.SceneManagement.Database
         public AssetReference GetSceneAssetReference();
 
         /// <summary>
+        /// Set target addressable scene.
+        /// </summary>
+        /// <param name="assetReference">Addressable scene asset reference.</param>
+        public void SetScene(AssetReference assetReference);
+        
+        #endregion
+
+        #region Scene Load Priotity Methods
+
+        /// <summary>
+        /// Set scene loading priority.
+        /// </summary>
+        /// <param name="sceneLoadPriority">Scene load priority.</param>
+        public void SetSceneLoadPriority(int sceneLoadPriority);
+        
+        /// <summary>
         /// Scene loading priority.
         /// </summary>
         public int GetSceneLoadPriority();
 
-        /// <summary>
-        /// Scene asset name.
-        /// </summary>
-        public string GetSceneAssetName();
+        #endregion
 
+        #region Scene Asset Type Methods
+
+        /// <summary>
+        /// Set scene asset type.
+        /// </summary>
+        /// <param name="sceneAssetType">Scene asset type to set.</param>
+        public void SetSceneAssetType(SceneAssetType sceneAssetType);
+        
         /// <summary>
         /// Scene asset type. Local or Addressable asset.
         /// </summary>
         public SceneAssetType GetSceneAssetType();
 
+        #endregion
+
+        #region Scene Load Type Methods
+
+        /// <summary>
+        /// Set scene load type.
+        /// </summary>
+        /// <param name="sceneLoadType">Scene load type to set.</param>
+        public void SetSceneLoadType(SceneLoadType sceneLoadType);
+        
         /// <summary>
         /// Scene load type. Active or Additional scene.
         /// </summary>
         public SceneLoadType GetSceneLoadType();
 
+        #endregion
+
+        #region Scene Load Mode Methods
+
+        /// <summary>
+        /// Set scene load mode.
+        /// </summary>
+        /// <param name="sceneLoadMode">Scene load mode to set.</param>
+        public void SetSceneLoadMode(LoadSceneMode sceneLoadMode);
+        
         /// <summary>
         /// Scene load mode. Single or Additive.
         /// </summary>
         public LoadSceneMode GetSceneLoadMode();
 
+        #endregion
+
+        #region Scene Acitvation Mode Methods
+
+        /// <summary>
+        /// Set scene activation mode.
+        /// </summary>
+        /// <param name="sceneActivationMode">Scene activation mode to set.</param>
+        public void SetSceneActivationMode(SceneActivationMode sceneActivationMode);
+        
         /// <summary>
         /// Scene activation mode. Auto or Manual.
         /// </summary>
         public SceneActivationMode GetActivationMode();
-
-        /// <summary>
-        /// Policy for scene reloading. Ignore or Allow scene reload.
-        /// </summary>
-        public SceneReloadPolicy GetSceneReloadPolicy();
 
         /// <summary>
         /// Cast activation mode enum to bool value.
@@ -60,10 +125,23 @@ namespace BurningLab.SceneManagement.Database
         /// <returns>True for auto activation scenes, false for manual activation scenes.</returns>
         public bool GetActivationModeAsBool();
         
+        #endregion
+
+        #region Scene Reload Policy Methods
+
         /// <summary>
-        /// Set target scene to scene data instance.
+        /// Set scene reload policy.
         /// </summary>
-        /// <param name="scenePath">Target scene path.</param>
-        public void SetScene(string scenePath);
+        /// <param name="sceneReloadPolicy">Scene reload policy to set.</param>
+        public void SetSceneReloadPolicy(SceneReloadPolicy sceneReloadPolicy);
+        
+        /// <summary>
+        /// Policy for scene reloading. Ignore or Allow scene reload.
+        /// </summary>
+        public SceneReloadPolicy GetSceneReloadPolicy();
+
+        #endregion
+        
+        #endregion
     }
 }
--- Assets/BurningLab/SceneManagement/Runtime/Database/Data/IScenesGroup.cs
+++ Assets/BurningLab/SceneManagement/Runtime/Database/Data/IScenesGroup.cs
@@ -8,10 +8,22 @@ namespace BurningLab.SceneManagement.Database
     public interface IScenesGroup
     {
         /// <summary>
+        /// Set scenes group name.
+        /// </summary>
+        /// <param name="scenesGroupName">Scenes group name to set.</param>
+        public void SetScenesGroupName(string scenesGroupName);
+        
+        /// <summary>
         /// Scenes group name.
         /// </summary>
         public string GetScenesGroupName();
 
+        /// <summary>
+        /// Set scenes group scenes lists.
+        /// </summary>
+        /// <param name="sceneReferences">List of scenes references to set.</param>
+        public void SetScenes(List<ISceneDataReference> sceneReferences);
+        
         /// <summary>
         /// Included scenes in scene group.
         /// </summary>
