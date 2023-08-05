d2c837acef29c3ed95ed64278f982dcb4a3c9dd8
n.fridman <n.fridman@burning-lab.com>
2023-08-05 07:39:07 +0300

    Update package name in scripts.

---
+++ Assets/BurningLab/SceneManagement/Samples/Minimal Setup Sample/Scripts/Providers/SimpleScenesManagementConfigurationProvider.cs
@@ -0,0 +1,24 @@
+﻿using BurningLab.SceneManagement.Database;
+using BurningLab.SceneManagement.Providers;
+using BurningLab.SceneManagement.Samples.MinimalSetupSample.Database;
+using UnityEngine;
+
+namespace BurningLab.SceneManagement.Samples.MinimalSetupSample.Providers
+{
+    /// <summary>
+    /// Provide reference to simple scenes management configuration instance.
+    /// </summary>
+    [AddTypeMenu("Simple Scenes Management Configuration Provider")]
+    [System.Serializable]
+    public class SimpleScenesManagementConfigurationProvider : IScenesManagementConfigurationProvider
+    {
+        [Tooltip("Target scenes management configuration.")]
+        [SerializeField] private SimpleScenesManagementConfiguration _targetConfiguration;
+
+        /// <summary>
+        /// Return reference to target scenes management configuration instance.
+        /// </summary>
+        /// <returns>Target scenes management configuration instance.</returns>
+        public IScenesManagementConfiguration GetConfiguration() => _targetConfiguration;
+    }
+}
